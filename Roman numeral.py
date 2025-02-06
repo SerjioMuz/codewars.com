4123a50adfe6188656e5e0ac437c66c6dc67374a
SerjioMuz <serjiomora911@gmail.com>
2025-02-06 16:29:37 +0200

    Roman Numerals Program for Checking 06-02-2025

---
+++ Roman numeral.py
@@ -0,0 +1,15 @@
+number=input('Введите число от 1 до 1000-->')
+res=[]; k=1
+l={1:'I', 2:'II', 3:'III', 5:'V', 10:'X', 50:'L', 100:'C', 500:'D', 1000:'M'}
+for i,o in enumerate(number,1):
+    dig=int(o+('0'*(len(number)-i)))  #Разбиваем число на сотни, десятки и еденицы
+    if dig<=(300/k):
+        res.append(l[(100/k)]*int(dig/(100/k)))
+    elif (900/k)<=dig:
+        res.append(l[(100/k)]*int(((1000/k)-dig)/(100/k))+l[(1000/k)])
+    elif dig<=(500/k):
+        res.append(l[(100/k)]*int(((500/k)-dig)/(100/k))+l[(500/k)])
+    elif (500/k)<dig:
+        res.append(l[(500/k)]+(l[(100/k)]*int((dig-(500/k))/(100/k))))
+    k=k*10   #Счетчик коэффициента для смены расчета сотен,десятков, едениц.
+print (''.join(res))
