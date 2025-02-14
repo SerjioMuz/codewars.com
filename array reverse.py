cb6998e5bdd777e4b5dc0f6812110b3e2e57fd74
SerjioMuz <serjiomora911@gmail.com>
2025-02-14 14:40:36 +0200

    task 2 (array reverse) the number of iterations has been halved

--- array reverse.py
+++ array reverse.py
@@ -12,7 +12,22 @@ def array_rewerse2(lis):
     return lis
 
 
+def array_rewerse3(lis):
+    lisRevers=[]
+    for index in range(len(lis)-1, -1, -2):
+        lisRevers.append(lis[index])
+        if index>0:
+            lisRevers.append(lis[index-1])
+    return lisRevers
 
 
+def array_rewerse4(lis):
+    for index in range(0, len(lis),2):
+        lis.insert(index,lis[-1])
+        del lis[-1]
+        lis.insert(index+1, lis[-1])
+        del lis[-1]
+    return lis
 
-print(array_rewerse2([1,2,3,4,5,6,'sdsd',7,8,9,0]))
+
+print(array_rewerse4([1,2,3,4,5,'j',6,7,8,9,0]))
