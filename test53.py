fba01f9797d9ba56569daa399bd945b72ea35d6f
SerjioMuz <serjiomora911@gmail.com>
2025-03-11 19:37:32 +0200

    Часть 4 Функции и генераторы Задание №6

--- test53.py
+++ test53.py
@@ -14,10 +14,35 @@ def copyDict(**dict):
         dict2.update({i:dict[i]})
 
     return dict2
+#print(copyDict(z='q', x='w',c='e', v='r'))
+
+def addDict (dict1, dict2):
+    if type(dict1)==list and type(dict2)==list:
+        return dict1+dict2
+    else:
+        dict3={}
+        for i in list(dict1.keys()):
+            dict3.update({i:dict1[i]})
+        for j in list(dict2.keys()):
+            dict3.update({j:dict2[j]})
+        return dict3
+
+
+print(addDict({4:'d',5:'e',6:'f'}, {1:'a',2:'b',3:'c'}))
+print(addDict([1,2,3],[4,5,6]))
 
 
 
 
 
 
-print(copyDict(z='q', x='w',c='e', v='r'))
+
+
+
+
+
+
+
+
+
+
