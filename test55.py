b49fa142df9907d2b830a971277063ee4ad2eecf
SerjioMuz <serjiomora911@gmail.com>
2025-04-10 15:43:40 +0300

    Часть 4 Функции и генераторы Задание №12 Завершение части 4.

---
+++ test55.py
@@ -0,0 +1,15 @@
+def countLines(location):
+    w='C:\\Users\\Serjio2\\AppData\\Local\\Programs\\Python\\Python313\\'
+    eee=w+location
+    print(eee)
+    myfile = open(r)
+    if myfile:
+        count=1
+        for line in myfile:
+            count+=1
+        return count
+
+
+
+print(countLines('data.txt'))
+
