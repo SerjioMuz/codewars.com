fd1a8bfb3e42b759561d3a9be56366f31ff63b20
SerjioMuz <serjiomora911@gmail.com>
2025-04-10 17:47:11 +0300

    Часть 6 Основы написания классов, подклассы.

--- classes in python.py
+++ classes in python.py
@@ -1,8 +1,22 @@
 class FirstClass:
-    def setdata(self, value, value2):
+    def setdata(self, value):
         self.data=value
-        self.data2 = value2
     def display(self):
-        print(self.data, self.data2)
+        print(self.data)
+
+
+class SecondClass(FirstClass):
+    def display(self):
+        print('Current value= "%s"' % self.data)
+
+class ThirdClass(SecondClass):
+    def __init__(self, value):
+        self.data = value
+    def __add__(self, other):
+        return ThirdClass(self.data+other)
+    def __str__(self):
+        return '[ThirdClass: %s]' % self.data
+    def mul(self, other):
+        self.data *=other
 
 
