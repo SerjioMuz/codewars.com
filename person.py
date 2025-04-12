337f8a7378ffd42d82aa625361aa29caf325631e
SerjioMuz <serjiomora911@gmail.com>
2025-04-12 15:49:07 +0300

    Часть 6, глава 28 Написание кода конструктора

--- person.py
+++ person.py
@@ -1,11 +1,21 @@
 class Person:
-	def __init__(self, name, job=None, pay=0):
-		self.name=name
-		self.job=job
-		self.pay=pay
+    def __init__(self, name, job=None, pay=0):
+        self.name = name
+        self.job = job
+        self.pay = pay
 
-bob=Person('Bob Smith')
-sue=Person('Sue Jones', job='dev', pay=100000)
+    def lastName(self):
+        return self.name.split()[-1]
 
-print(bob.name, bob.pay)
-print(sue.name, sue.pay)
+    def giveRaise(self, persent):
+        self.pay = int(self.pay * (1 + persent))
+
+
+if __name__ == '__main__':
+    bob = Person('Bob Smith')
+    sue = Person('Sue Jones', job='dev', pay=100000)
+    print(bob.name, bob.pay)
+    print(sue.name, sue.pay)
+    print(bob.lastName(), sue.lastName())
+    sue.giveRaise(.10)
+    print(sue.pay)
