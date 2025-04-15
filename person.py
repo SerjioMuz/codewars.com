068ada69e971ea28c3641efe36f90b4f9f472d85
SerjioMuz <serjiomora911@gmail.com>
2025-04-15 12:39:13 +0300

    Часть 6, глава 28 Написание кода конструктора

--- person.py
+++ person.py
@@ -3,16 +3,12 @@ class Person:
         self.name = name
         self.job = job
         self.pay = pay
-
     def lastName(self):
         return self.name.split()[-1]
-
     def giveRaise(self, persent):
         self.pay = int(self.pay * (1 + persent))
-
     def __repr__(self):
         return '[Person: %s, %s]' % (self.name, self.pay)
-
 
 class Manager(Person):
     def __init__(self, name, pay):
@@ -21,16 +17,31 @@ class Manager(Person):
     def giveRaise(self, persent, bonus=.10):
         Person.giveRaise(self, persent + bonus)
 
+class Department:
+    def __init__(self, *args):
+        self.members=list(args)
+    def addMember(self, person):
+        self.members.append(person)
+    def giveRaises(self, persent):
+        for person in self.members:
+            person.giveRaise(persent)
+    def showAll(self):
+        for person in self.members:
+            print(person)
 
 if __name__ == '__main__':
     bob = Person('Bob Smith')
     sue = Person('Sue Jones', job='dev', pay=100000)
-    print(bob.name, bob.pay)
-    print(sue.name, sue.pay)
-    print(bob.lastName(), sue.lastName())
-    sue.giveRaise(.10)
-    print(sue)
+    #print(bob.name, bob.pay)
+    #print(sue.name, sue.pay)
+    #print(bob.lastName(), sue.lastName())
+    #sue.giveRaise(.10)
+    #print(sue)
     tom = Manager('Tom Jones', 50000)
-    tom.giveRaise(.10)
-    print(tom.lastName())
-    print(tom)
+    #tom.giveRaise(.10)
+    #print(tom.lastName())
+    #print(tom)
+    development=Department(bob, sue)
+    development.addMember(tom)
+    development.giveRaises(.10)
+    development.showAll()
