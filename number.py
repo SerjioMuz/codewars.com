c6c6f92ab0b6fb5771c50b3cbf8856cb5dd72897
SerjioMuz <serjiomora911@gmail.com>
2025-04-22 13:48:56 +0300

    Часть 6, глава 30. Перегрузка операций. Lesson 1

--- number.py
+++ number.py
@@ -3,3 +3,8 @@ class Number:
         self.data=start
     def __sub__(self, other):
         return Number(self.data-other)
+
+class Indexer:
+    def __getitem__(self, index):
+        return index**2
+
