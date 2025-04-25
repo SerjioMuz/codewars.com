d23442547d87203018067bc79a145460ef4664ca
SerjioMuz <serjiomora911@gmail.com>
2025-04-25 10:26:41 +0300

    Часть 6, глава 30-2 Перегрузка операций. Итерируемые объекты Lesson 3

--- Lesson30-3.py
+++ Lesson30-3.py
@@ -37,3 +37,11 @@ class Squares:
     def __iter__(self):
         for value in range(self.start, self.stop+1):
             yield value **2
+
+class Squares:
+    def __init__(self, start, stop):
+        self.start=start
+        self.stop=stop
+    def gen(self):
+        for value in range(self.start, self.stop+1):
+            yield value **2
