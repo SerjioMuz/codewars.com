986aa63c7c7c536356ebca805d179235b87937cb
SerjioMuz <serjiomora911@gmail.com>
2025-04-25 11:11:49 +0300

    Часть 6, глава 30-2 Перегрузка операций. Итерируемые объекты Lesson 3

--- Lesson30-3.py
+++ Lesson30-3.py
@@ -45,3 +45,31 @@ class Squares:
     def gen(self):
         for value in range(self.start, self.stop+1):
             yield value **2
+
+"""Генератор не основанный на yield"""
+class Squares:
+    def __init__(self, start, stop):
+        self.start=start
+        self.stop=stop
+    def __iter__(self):
+        return SquaresIter(self.start, self.stop)
+class SquaresIter:
+    def __init__(self, start, stop):
+        self.value=start-1
+        self.stop=stop
+    def __next__(self):
+        if self.value==self.stop:
+            raise StopIteration
+        self.value+=1
+        return self.value**2
+
+"""Генератор на основе __iter__ + yield"""
+class SkipObject:
+    def __init__(self, wrapped):
+        self.wrapped=wrapped
+    def __iter__(self):
+        offset=0
+        while offset < len(self.wrapped):
+            item=self.wrapped[offset]
+            offset+=2
+            yield item
