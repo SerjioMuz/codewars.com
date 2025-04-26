7fc74975483d09759bbba48a05fd101f2ed3bd48
SerjioMuz <serjiomora911@gmail.com>
2025-04-26 13:25:28 +0300

    Часть 6, глава 30-2 Перегрузка операций. Членство: __contains__, __iter__ __getitem__ Lesson 4

--- lesson30-4.py
+++ lesson30-4.py
@@ -5,20 +5,20 @@ class Iters:
     def __getitem__(self, i):
         print('get[%s]:' % i, end=' ')
         return self.data[i]
-"""    def __iter__(self):
+    def __iter__(self):
         print('iter=> ', end=' ')
         self.ix=0
-        return self"""
+        return self
     def __next__(self):
         print('next:', end=' ')
         if self.ix==len(self.data): raise StopIteration
         item=self.data[self.ix]
         self.ix +=1
         return item
-"""    def __contains__(self, x):
+    def __contains__(self, x):
         print('contains: ', end=' ')
         return x in self.data
-        next=__next__"""
+        next=__next__
 
 if __name__=='__main__':
     x=Iters([1,2,3,4,5])
