ea1902c6d76ecbd26221709a88985d8bae30fa6a
SerjioMuz <serjiomora911@gmail.com>
2025-05-02 21:12:43 +0300

    Часть 6, глава 30 Перегрузка операций. Строковое представление: __iadd__, __add__ Lesson 10

--- lesson30-10.py
+++ lesson30-10.py
@@ -11,3 +11,32 @@ class Number:
         self.val=val
     def __add__(self, other):
         return Number(self.val+other)
+
+class Callee:
+    def __call__(self, *pargs, **kargs):
+        print('Called:' , pargs, kargs)
+
+class Prog:
+    def __init__(self, value):
+        self.value=value
+    def __call__(self, other):
+        return self.value * other
+
+class Prog:
+    def __init__(self, value):
+        self.value=value
+    def comp(self, other):
+        return self.value * other
+
+class Callback:
+    def __init__(self, color):
+        self.color=color
+    def __call__(self):
+        print('turn', self.color)
+
+def callback(color):
+    def oncall():
+        print('turn', color)
+    return oncall
+
+
