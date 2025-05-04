68f5c7d77927fd0e05d87ac53ef1d982e1c98177
SerjioMuz <serjiomora911@gmail.com>
2025-05-04 15:48:45 +0300

    Часть 6, глава 30 Перегрузка операций. Строковое представление: __iadd__, __add__ Lesson 10

--- lesson30-10.py
+++ lesson30-10.py
@@ -39,4 +39,9 @@ def callback(color):
         print('turn', color)
     return oncall
 
-
+class C:
+    data='spam'
+    def __gt__(self, other):
+        return self.data>other
+    def __lt__(self, other):
+        return self.data<other
