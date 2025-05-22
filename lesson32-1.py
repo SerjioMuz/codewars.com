3f98b63e4e4a71de8fb1065d64488de3d32f78b0
SerjioMuz <serjiomora911@gmail.com>
2025-05-22 12:20:33 +0300

    Часть 6, глава 32 Расширенные возможности классов. Расширение встроенных типов.

--- lesson32-1.py
+++ lesson32-1.py
@@ -20,10 +20,24 @@ class Set:
             if not x in self.data:
                 self.data.append(x)
 
-
 def __len__(self): return len(self.data)
 def __getitem__(self, key): return self.data[key]
 def __and__(self, other): return self.intersect(other)
 def __or__(self, other): return self.union(other)
 def __repr__(self): return "Set:" + repr(self.data)
 def __item__(self): return iter(self.data)
+
+
+"Файл tupesub-class.py"
+class MyList(list):
+    def __getitem__(self, offset):
+        print('(indexing %s at %s)' % (self, offset))
+        return list.__getitem__(self, offset-1)
+if __name__=='__main__':
+    print(list('abc'))
+    x=MyList('abc')
+    print(x)
+    print(x[1])
+    print(x[3])
+    x.append('spam'); print(x)
+    x.reverse(); print(x)
