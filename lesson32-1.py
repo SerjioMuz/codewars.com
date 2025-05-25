1347e51391aeea4ef3e67848104e24791e2768eb
SerjioMuz <serjiomora911@gmail.com>
2025-05-25 17:49:11 +0300

    Часть 6, глава 32 Расширенные возможности классов. Расширение встроенных типов.

--- lesson32-1.py
+++ lesson32-1.py
@@ -82,4 +82,11 @@ class C(object):
     def __getattr__(self, name):
         print('getattr:' + name)
         return getattr(self.data, name)
+    def __getitem__(self, i):
+        print('getitem: '+str(i))
+        return self.data[i]
+    def __add__(self, other):
+        print('add: '+other)
+        return getattr(self.data, '__add__')(other)
+
 
