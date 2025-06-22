2f546f8b1c2a26f6909defb10e46bdb9a932b91d
SerjioMuz <serjiomora911@gmail.com>
2025-06-22 12:02:46 +0300

    Часть 6, глава 32 Расширенные возможности классов. Расширение встроенных типов.

--- lesson32-4.py
+++ lesson32-4.py
@@ -65,3 +65,13 @@ class Sub(Spam):
         print('Extra stuff...')
         Spam.printNumInstances()
     printNumInstances=staticmethod(printNumInstances)
+
+
+class Spam:
+    numInstances=0
+    def __init__(self):
+        Spam.numInstances +=1
+    def printNumInstances(cls):
+        print('Number of instances: %s' % cls.numInstances)
+    printNumInstances=classmethod(printNumInstances)
+
