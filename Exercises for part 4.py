793e17494cef1e508e26ae3b0de7ab5725f3b4f1
SerjioMuz <serjiomora911@gmail.com>
2025-07-20 16:27:45 +0300

    Упражнения для части 4

--- Exercises for part 4.py
+++ Exercises for part 4.py
@@ -131,3 +131,31 @@ class MultiSet(Set):
                     res.append(x)
         return Set(res)
 
+
+
+
+class ListInstance:
+    def __attrnames(self):
+        result=''
+        for attr in sorted(self.__dict__):
+            result += '\t%s=%s\n' % (attr, self.__dict__[attr])
+        return result
+    def __str__(self):
+        return '<Instanse of %s(%s), address %s:\n%s>' % (
+            self.__class__.__name__,
+            self.__supers(),
+            id(self),
+            self.__attrnames())
+    def __supers(self):
+        names=[]
+        for super in self.__class__.__bases__:
+            names.append(super.__name__)
+        return', '.join(names)
+
+
+if __name__=='__main__':
+    import testmixin
+    testmixin.tester(ListInstance)
+
+
+
