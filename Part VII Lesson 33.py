a8f7cbc36a11c84bfce5bfe0eceba13c329d44e0
SerjioMuz <serjiomora911@gmail.com>
2025-09-20 17:07:11 +0300

    Упражнения для части 4

---
+++ Part VII Lesson 33.py
@@ -0,0 +1,35 @@
+class AlreadyGotOne(Exception): pass
+
+def grail():
+    raise AlreadyGotOne()
+
+try:
+    grail()
+except AlreadyGotOne:
+    print('got exception')
+
+
+
+
+class Carrer(Exception):
+    def __str__(self): return 'So I became a waited...'
+
+
+def after():
+    try:
+        fetcher(x,3)
+    finally:
+        print('after fetcher')
+    print('after try')
+
+
+def kaboom(x,y):
+    print(x+y)
+try:
+    kaboom([1,2,3], 'spam')
+except TypeError:
+    print('erroorrs')
+print('no errors')
+
+
+
