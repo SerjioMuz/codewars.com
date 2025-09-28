f6f4e0acb12478915f6ea687ff8eb0556420afec
SerjioMuz <serjiomora911@gmail.com>
2025-09-28 19:59:42 +0300

    Упражнения для части 4

---
+++ Part VII Lesson 35.py
@@ -0,0 +1,19 @@
+"Файл classexc.py"
+class General(Exception): pass
+class Specific1(General): pass
+class Specific2(General): pass
+def raiser0():
+    x=General()
+    raise x
+def raiser1():
+    x=Specific1()
+    raise x
+def raiser2():
+    x=Specific2()
+    raise x
+for func in (raiser0, raiser1, raiser2):
+    try:
+        func()
+    except General:
+        import sys
+        print('caught: %s' % sys.exc_info()[0])
