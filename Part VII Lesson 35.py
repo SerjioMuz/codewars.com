4a6c68c3887aa14388a14c2372a619490332587d
SerjioMuz <serjiomora911@gmail.com>
2025-09-28 20:35:55 +0300

    Упражнения для части 4

--- Part VII Lesson 35.py
+++ Part VII Lesson 35.py
@@ -17,3 +17,17 @@ for func in (raiser0, raiser1, raiser2):
     except General:
         import sys
         print('caught: %s' % sys.exc_info()[0])
+
+"Файл classexc2.py"
+class General(Exception): pass
+class Specific1(General): pass
+class Specific2(General): pass
+def raiser0(): raise General()
+def raiser1(): raise Specific1()
+def raiser2(): raise Specific2()
+for func in (raiser0, raiser1, raiser2):
+    try:
+        func()
+    except General as x:
+        print('caught: %s' % x.__class__)
+
