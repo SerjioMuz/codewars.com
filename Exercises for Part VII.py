d873b1c35117aa3a9ec78300e064b138570eed0d
SerjioMuz <serjiomora911@gmail.com>
2025-10-01 20:28:29 +0300

    Упражнения для части 4

--- Exercises for Part VII.py
+++ Exercises for Part VII.py
@@ -21,3 +21,32 @@ def doomed():
         print('ERROR2',MyError, x)
     else:
         print('OK')
+
+"Exercise 3"
+import sys, traceback
+def safe(calle, *pargs, **kargs):
+    try:
+        calle (*pargs, ** kargs)
+    except:
+        traceback.print_exc()
+        print ('Got %s, %s' % (sys.exc_info()[0], sys.exc_info()[1]))
+if __name__=='__main__':
+    import oops2
+    safe(oops2.oops)
+
+import sys, traceback
+def safe(calle):
+    def callproxy(*pargs, **kargs):
+        try:
+            return calle(*pargs, **kargs)
+        except:
+            traceback.print_exc()
+            print('Got %s, %s' % (sys.exc_info()[0], sys.exc_info()[1]))
+            raise
+    return callproxy
+if __name__=='__main__':
+    import oops2
+    @safe
+    def test():
+        oops2.oops()
+    test()
