c4ab83786ed94b0216aa355cfe9976daed197d2c
SerjioMuz <serjiomora911@gmail.com>
2025-10-01 16:53:27 +0300

    Упражнения для части 4

--- Exercises for Part VII.py
+++ Exercises for Part VII.py
@@ -8,3 +8,16 @@ except KeyError:
 else:
     print('OK')
 
+"Exercise 2"
+class MyError(Exception): pass
+def oops():
+    raise MyError('Spam')
+def doomed():
+    try:
+        oops()
+    except IndexError:
+        print ("ERROR1")
+    except MyError as x:
+        print('ERROR2', x)
+    else:
+        print('OK')
