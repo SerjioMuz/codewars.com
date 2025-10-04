bbaab029383872d4dd44302c62d9952d64e9efd3
SerjioMuz <serjiomora911@gmail.com>
2025-10-04 20:27:00 +0300

    Примеры для самообучения

---
+++ examples for self-study.py
@@ -0,0 +1,52 @@
+"Exercise 1 Поиск самого крупного файла с исходным кодом Python в отдельно взятом каталоге"
+import os, glob
+dirname=r'C:\Users\Serjio2\AppData\Local\Programs\Python\Python313\Lib'
+allsize=[]
+allpy=glob.glob(dirname + os.sep+'*.py')
+for filename in allpy:
+    filesize=os.path.getsize(filename)
+    allsize.append((filesize, filename))
+allsize.sort()
+print(allsize[:2])
+print(allsize[-2:])
+
+"Exercise 2 Поиск самого крупного файла с исходным кодом Python в дереве каталогов"
+import sys, os, pprint
+if sys.platform[:3]=='win':
+    dirname=r'C:\Users\Serjio2\AppData\Local\Programs\Python\Python313\Lib'
+else:
+    dirname='/usr/lib/python'
+allsize=[]
+for (thisDir, subsHere, filesHere) in os.walk(dirname):
+    for filename in filesHere:
+        if filename.endswith('.py'):
+            fullname=os.path.join(thisDir, filename)
+            fullsize=os.path.getsize(fullname)
+            allsize.append((fullsize, fullname))
+allsize.sort()
+pprint.pprint(allsize[:2])
+pprint.pprint(allsize[-2:])
+
+"Exercise 2 Поиск самого крупного файла с исходным кодом Python в пути поиска импортируемых модулей"
+import sys, os, pprint
+visited={}
+allsizes=[]
+for crsdir in sys.path:
+    for (thisDir, subsHere, filesHere) in os.walk(crsdir):
+        thisDir=os.path.normpath(thisDir)
+        if thisDir.upper() in visited:
+            continue
+        else:
+            visited[thisDir.upper()]=True
+        for filename in filesHere:
+            if filename.endswith('.py'):
+                pypath=os.path.join(thisDir, filename)
+                try:
+                    pysize=os.path.getsize(pypath)
+                except:
+                    print('skipping', pypath)
+                allsizes.append((pysize, pypath))
+allsizes.sort()
+pprint.pprint(allsize[:3])
+pprint.pprint(allsize[-3:])
+
