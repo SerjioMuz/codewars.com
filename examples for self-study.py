95cb8f988465ba594ea7e6949fdce5361bb563e5
SerjioMuz <serjiomora911@gmail.com>
2025-10-05 19:39:51 +0300

    Примеры для самообучения

--- examples for self-study.py
+++ examples for self-study.py
@@ -94,3 +94,35 @@ for testcase in testscripts:
             print('Passed:', testcase['script'])
 
 
+"Exercise 6 Построение графического пользовательского интерфейса"
+from tkinter import *
+import random
+fontsize=25
+colors=['red', 'green', 'blue', 'yellow', 'orange', 'white', 'cyan', 'purple']
+
+def reply(text):
+    print(text)
+    popup=Toplevel()
+    color=random.choice(colors)
+    Label(popup, text='Popup', bg='black', fg=color).pack()
+    L.config(fg=color)
+
+def timer():
+    L.config(fg=random.choise(colors))
+    win.after(250, timer)
+
+def grow():
+    global fontsize
+    fontsize +=5
+    L.config(font=('arial', fontsize, 'italic'))
+    win.after(100, grow)
+
+win=Tk()
+L=Label(win, text='spam',
+        font=('arial', fontsize, 'italic'), fg='yellow', bg='navy',
+        relief=RAISED)
+L.pack(side=TOP, expand=YES, fill=BOTH)
+Button(win, text="press", command=(lambda: reply('red'))).pack(side=BOTTOM, fill=X)
+Button(win, text='timer', command='timer').pack(side=BOTTOM, fill=X)
+Button(win, text='grow', command='grow').pack(side=BOTTOM, fill=X)
+win.mainloop()
