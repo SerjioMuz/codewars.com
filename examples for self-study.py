c562084b385d24cae26955ae273da35f8a6e81db
SerjioMuz <serjiomora911@gmail.com>
2025-10-05 12:46:39 +0300

    Примеры для самообучения

--- examples for self-study.py
+++ examples for self-study.py
@@ -50,3 +50,13 @@ allsizes.sort()
 pprint.pprint(allsizes[:3])
 pprint.pprint(allsizes[-3:])
 
+"Exercise 3 Суммирование столбцов в текстовом файле с разделителями-запятыми"
+filename='data.txt'
+sums={}
+for line in open(filename):
+    cols=line.split(',')
+    nums=[int(col) for col in cols]
+    for (ix, num) in enumerate(nums):
+        sums[ix]=sums.get(ix,0)+num
+for key in sorted(sums):
+    print(key, '=', sums[key])
