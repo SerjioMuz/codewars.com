94c927ff84cda07e7522888a572d5f07e60f21e4
SerjioMuz <serjiomora911@gmail.com>
2025-10-17 16:38:13 +0300

    Примеры для самообучения

--- Exercises for Part VIII.py
+++ Exercises for Part VIII.py
@@ -49,3 +49,16 @@ for (i, c) in enumerate (t) :
     except:
         print(i, sys.exc_infо()[0])
 
+class Person:
+    def __init__ (self, name):
+        self._name = name
+    def getName(self):
+        print('fetch...')
+        return self._name
+    def setName(self, value):
+        print('change...')
+        self._name = value
+    def delName(self) :
+        print('remove...')
+        del self._name
+    name = property(getName, setName, delName, "name property docs")
