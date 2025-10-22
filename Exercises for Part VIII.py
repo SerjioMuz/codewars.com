41e89e71d45c74a555a6f78582edadd0591e1727
SerjioMuz <serjiomora911@gmail.com>
2025-10-22 19:54:31 +0300

    Примеры для самообучения

--- Exercises for Part VIII.py
+++ Exercises for Part VIII.py
@@ -225,22 +225,22 @@ class Wrapper:
 
 
 class Person:
-    def init (self, name):
+    def __init__ (self, name):
         self._name = name
     def __getattr__ (self, attr):
-        print('get:' + attr)
+        print('get:' ,attr)
         if attr == 'name':
             return self._name
         else:
             raise AttributeError(attr)
     def __setattr__ (self, attr, value):
-        print ('set:' + attr)
-        if attr == ' name ':
+        print ('set:' ,attr)
+        if attr == 'name':
             attr = '_name'
-        self.__diet__[attr] = value
+        self.__dict__[attr] = value
     def __delattr__(self, attr):
-        print('del:' + attr)
-        if attr == ' name ' :
+        print('del:' ,attr)
+        if attr == 'name' :
             attr = '_name'
             del self.__dict__ [attr]
 
