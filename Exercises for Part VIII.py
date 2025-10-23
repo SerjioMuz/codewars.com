2eb0168f0d953a7492442c7be912dfa3bf1d529f
SerjioMuz <serjiomora911@gmail.com>
2025-10-23 12:05:30 +0300

    Примеры для самообучения

--- Exercises for Part VIII.py
+++ Exercises for Part VIII.py
@@ -244,3 +244,37 @@ class Person:
             attr = '_name'
             del self.__dict__ [attr]
 
+
+class Person:
+    def __init__ (self, name):
+        self._name = name
+    def __getattribute__ (self, attr):
+        print('get:' , attr)
+        if attr == 'name':
+            attr = '_name'
+        return object.__getattribute__(self, attr)
+
+    def __setattr__ (self, attr, value):
+        print ('set:' ,attr)
+        if attr == 'name':
+            attr = '_name'
+        self.__dict__[attr] = value
+    def __delattr__(self, attr):
+        print('del:' ,attr)
+        if attr == 'name' :
+            attr = '_name'
+            del self.__dict__ [attr]
+
+
+class AttrSquare:
+    def __init__ (self, start):
+        self.value = start
+    def __getattr__ (self, attr):
+        if attr == 'X':
+            return self.value ** 2
+        else:
+            raise AttributeError(attr)
+    def __setattr__(self, attr, value):
+        if attr == 'X':
+            attr = 'value'
+        self.__dict__[attr] = value
