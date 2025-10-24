de968f275a0f5d138c7c80f1027a2bebd8383630
SerjioMuz <serjiomora911@gmail.com>
2025-10-24 19:54:02 +0300

    Примеры для самообучения

--- Exercises for Part VIII.py
+++ Exercises for Part VIII.py
@@ -440,3 +440,41 @@ print(X.cube)
 X.square=5
 print(X.square)
 
+class GetAttr:
+    eggs = 88
+    def __init__ (self) :
+        self.spam = 77
+    def __len__ (self):
+        print('__len__ : 42')
+        return 42
+    def __getattr__ (self, attr) :
+        print ('getattr: ' + attr)
+        if attr == '__str__ ':
+            return lambda *args: '[Getattr str]'
+        else:
+            return lambda *args: None
+
+
+class GetAttribute:
+    eggs = 88
+    def __init__ (self):
+        self.spam = 77
+    def __len__ (self):
+        print ('__len__ : 42')
+        return 42
+    def __getattribute__(self, attr):
+        print ('getattribute: ' + attr)
+        if attr == '__str__ ':
+            return lambda *args: ' [GetAttribute str]'
+        else:
+            return lambda *args: None
+
+#for Class in GetAttr, GetAttribute:
+#    print('\n' + Class.__name__.ljust(50, '='))
+X = GetAttribute()
+X.eggs
+X.spam
+X.other
+len(X)
+try: X[0]
+except: print('fail []')
