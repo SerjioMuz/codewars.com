0b90e28ead09412f98c2785bffea6f2677aea5fb
SerjioMuz <serjiomora911@gmail.com>
2025-10-25 13:37:57 +0300

    Примеры для самообучения

--- Exercises for Part VIII.py
+++ Exercises for Part VIII.py
@@ -513,3 +513,75 @@ if __name__ == '__main__' :
     print (tom)
 
 
+class Person:
+    def __init__ (self, name, job=None, pay=0):
+        self.name = name
+        self.job = job
+        self.pay = pay
+    def lastName(self):
+        return self.name.split () [-1]
+    def giveRaise(self, percent):
+        self.pay = int(self.pay * (1 + percent))
+    def __repr__ (self):
+        return ' [Person: %s, %s] ' % (self.name, self.pay)
+
+class Manager:
+    def __init__ (self, name, pay):
+        self.person = Person(name, 'mgr', pay)
+    def giveRaise(self, percent, bonus=.10):
+        self.person.giveRaise (percent + bonus)
+    def __getattribute__(self, attr):
+        print('**', attr)
+        if attr in ['person', 'giveRaise']:
+            return object.__getattribute__(self, attr)
+        else:
+            return getattr(self.person, attr)
+#    def __repr__ (self):
+#        return str(self.person)
+
+if __name__ == '__main__' :
+    sue = Person('Sue Jones', job='dev(', pay=100000)
+    print(sue.lastName())
+    sue.giveRaise(.10)
+    print(sue)
+    tom = Manager('Tom Jones', 50000)
+    print(tom.lastName())
+    tom.giveRaise(.10)
+    print (tom)
+
+
+class Person:
+    def __init__ (self, name, job=None, pay=0):
+        self.name = name
+        self.job = job
+        self.pay = pay
+    def lastName(self):
+        return self.name.split () [-1]
+    def giveRaise(self, percent):
+        self.pay = int(self.pay * (1 + percent))
+    def __repr__ (self):
+        return ' [Person: %s, %s] ' % (self.name, self.pay)
+
+class Manager:
+    def __init__ (self, name, pay):
+        self.person = Person(name, 'mgr', pay)
+    def __getattribute__(self, attr):
+        print('**', attr)
+        person=object.__getattribute__(self, 'person')
+        if attr == 'giveRaise':
+            return lambda percent: person.giveRaise(percent+.10)
+        else:
+            return getattr(person, attr)
+    def __repr__ (self):
+        person=object.__getattribute__(self, 'person')
+        return str(person)
+
+if __name__ == '__main__' :
+    sue = Person('Sue Jones', job='dev(', pay=100000)
+    print(sue.lastName())
+    sue.giveRaise(.10)
+    print(sue)
+    tom = Manager('Tom Jones', 50000)
+    print(tom.lastName())
+    tom.giveRaise(.10)
+    print (tom)
