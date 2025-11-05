e15c5de6a9e9c758071ed8a840a37eecaf81f317
SerjioMuz <serjiomora911@gmail.com>
2025-11-05 20:27:56 +0200

    Декораторы функций

---
+++ Exercises for Part VIII Chapter 39 Decorators.py
@@ -0,0 +1,64 @@
+def d1(F): return(F)
+def d2(F): return(F)
+def d3(F): return(F)
+
+@d1
+@d2
+@d3
+def func():
+    print('spam')
+func()
+
+
+def d1(F):
+    print(1)
+    return lambda: 'x' + F()
+def d2(F):
+    print(2)
+    return lambda: 'y' + F()
+def d3(F):
+    print(3)
+    return lambda: 'z' + F()
+
+@d1
+@d2
+@d3
+def func():
+    print(4)
+    return 'spam'
+print(func())
+
+"Файл decorator1.py"
+class tracer:
+    def __init__(self, func):
+        self.calls=0
+        self.func=func
+    def __call__(self, *args):
+        self.calls +=1
+        print('call %s to %s' % (self.calls, self.func.__name__))
+        self.func(*args)
+@tracer
+def spam(a,b,c):
+    print(a+b+c)
+
+@tracer
+def spam1(a,b):
+    print(a+b)
+
+@tracer
+def spam2(a):
+    print(a)
+
+
+
+
+calls=0
+def tracer(func, *args):
+    global calls
+    calls += 1
+    print ('call %s to %s' % (calls, func.__name__))
+    func(*args)
+def spam(a, b, c):
+    print(a, b, c)
+def spam1(a, b, c):
+    print(a, b, c)
