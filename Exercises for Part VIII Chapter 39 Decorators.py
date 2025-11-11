601d533d6ef87c5d9476a8933f590cdf7a788963
SerjioMuz <serjiomora911@gmail.com>
2025-11-11 14:53:59 +0200

    Декораторы функций

--- Exercises for Part VIII Chapter 39 Decorators.py
+++ Exercises for Part VIII Chapter 39 Decorators.py
@@ -83,4 +83,83 @@ def eggs(x,y):
 
 
 
+calls=0
+def tracer(func):
+    def wrapper(*args, **kwargs):
+        global calls
+        calls+=1
+        print('call %s to %s' % (calls, func.__name__))
+        return func(*args, **kwargs)
+    return wrapper
+@tracer
+def spam(a, b, c) :
+    print (a + b + c)
 
+@tracer
+def eggs(x,y):
+    print(x ** y)
+
+
+
+def tracer(func):
+    calls = 0
+    def wrapper(*args, **kwargs):
+        nonlocal calls
+        calls+=1
+        print('call %s to %s' % (calls, func.__name__))
+        return func(*args, **kwargs)
+    return wrapper
+@tracer
+def spam(a, b, c) :
+    print (a + b + c)
+
+@tracer
+def eggs(x,y):
+    print(x ** y)
+
+
+
+def tracer(func):
+
+    def wrapper(*args, **kwargs):
+        wrapper.calls += 1
+        print('call %s to %s' % (wrapper.calls, func.__name__))
+        return func(*args, **kwargs)
+    wrapper.calls=0
+    return wrapper
+@tracer
+def spam(a, b, c) :
+    print (a + b + c)
+
+@tracer
+def eggs(x,y):
+    print(x ** y)
+
+
+
+class tracer:
+    def __init__ (self, func):
+        self .calls = 0
+        self. func = func
+    def __call__ (self, *args, **kwargs):
+        self.calls += 1
+        print('call %s to %s' % (self.calls, self.func.__name__))
+        return self.func(*args, **kwargs)
+@tracer
+def spam(a, b, с) :
+    print(a + b + с)
+
+
+
+class Person:
+    def __init__(self, name, pay):
+        self.name= name
+        self.pay= pay
+
+@tracer
+def giveRaise(self, percent):
+    self.pay *= (0.1+percent)
+
+@tracer
+def lastName(self):
+    return self.name.split()[-1]
