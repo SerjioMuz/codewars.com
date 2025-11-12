211240244c11da116a8dfddb639e7fba22ca4a6d
SerjioMuz <serjiomora911@gmail.com>
2025-11-12 21:04:45 +0200

    Декораторы функций

--- Exercises for Part VIII Chapter 39 Decorators.py
+++ Exercises for Part VIII Chapter 39 Decorators.py
@@ -1,3 +1,6 @@
+from curses import wrapper
+
+
 def d1(F): return(F)
 def d2(F): return(F)
 def d3(F): return(F)
@@ -156,10 +159,123 @@ class Person:
         self.name= name
         self.pay= pay
 
-@tracer
-def giveRaise(self, percent):
-    self.pay *= (0.1+percent)
+    @tracer
+    def giveRaise(self, percent):
+        self.pay *= (0.1+percent)
+
+    @tracer
+    def lastName(self):
+        return self.name.split()[-1]
+
+
+
+def tracer(func):
+    calls = 0
+    def onCall(*args, **kwargs):
+        nonlocal calls
+        calls+=1
+        print('call %s to %s' % (calls, func.__name__))
+        return func(*args, **kwargs)
+    return onCall
 
 @tracer
-def lastName(self):
-    return self.name.split()[-1]
+def spam(a,b,c):
+    print(a,b,c)
+
+@tracer
+def eggs(N):
+    return 2**N
+
+spam(1,2,3)
+spam(a=4, b=5, c=6)
+print(eggs(32))
+
+
+
+
+
+
+class tracer:
+    def __init__ (self, func):
+        self .calls = 0
+        self. func = func
+    def __call__ (self, *args, **kwargs):
+        self.calls += 1
+        print('call %s to %s' % (self.calls, self.func.__name__))
+        return self.func(*args, **kwargs)
+    def __get__(self, instance, owner):
+        return wrapper(self,instance)
+
+class wrapper:
+    def __init__(self, desc, subj):
+        self.desc=desc
+        self.subj=subj
+    def __call__(self, *args, **kwargs):
+        return self.desc(self.subj, *args, **kwargs)
+
+@tracer
+def spam(a,b,c):
+    print(a,b,c)
+
+class Person:
+    def __init__(self, name, pay):
+        self.name= name
+        self.pay= pay
+
+    @tracer
+    def giveRaise(self, percent):
+        self.pay *= (1.0+percent)
+
+    @tracer
+    def lastName(self):
+        return self.name.split()[-1]
+
+
+
+
+lass tracer:
+    def __init__(self, func):
+        self.calls = 0
+        self.func = func
+
+    def __call__(self, *args, **kwargs):
+        self.calls += 1
+        print('call %s to %s' % (self.calls, self.func.__name__))
+        return self.func(*args, **kwargs)
+
+    def __get__(self, instance, owner):
+        def wrapper(*args, **kwargs):
+            return self(instance, *args, **kwargs)
+        return wrapper
+
+@tracer
+def spam(a, b, c):
+    print(a, b, c)
+
+
+class Person:
+    def __init__(self, name, pay):
+        self.name = name
+        self.pay = pay
+
+    @tracer
+    def giveRaise(self, percent):
+        self.pay *= (1.0 + percent)
+
+    @tracer
+    def lastName(self):
+        return self.name.split()[-1]
+
+
+
+spam(1,2,3)
+spam(a=4, b=5, c=6)
+
+
+print('methods')
+bob=Person('Bob Smith', 50000)
+sue=Person('Sue Jonson', 100000)
+print(bob.name, sue.name)
+sue.giveRaise(.10)
+print(int(sue.pay))
+print(bob.lastName(), sue.lastName())
