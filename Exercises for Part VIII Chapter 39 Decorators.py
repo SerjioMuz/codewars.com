95489e1dc50ad91fe4f0284ff47b4dceda027fe3
SerjioMuz <serjiomora911@gmail.com>
2025-11-15 21:18:09 +0200

    Декораторы функций

--- Exercises for Part VIII Chapter 39 Decorators.py
+++ Exercises for Part VIII Chapter 39 Decorators.py
@@ -279,3 +279,45 @@ print(bob.name, sue.name)
 sue.giveRaise(.10)
 print(int(sue.pay))
 print(bob.lastName(), sue.lastName())
+
+
+
+
+"Файл timerdeco1.py"
+import time
+class timer:
+    def __init__(self, func):
+        self.func=func
+        self.alltime=0
+    def __call__(self, *args, **kwargs):
+        start=time.perf_counter()
+        result=self.func(*args, **kwargs)
+        elapsed=time.perf_counter() - start
+        self.alltime += elapsed
+        print('%s: %.5f, %.5f' % (self.func.__name__, elapsed, self.alltime))
+        return result
+
+@timer
+def listcomp(N):
+    return [x*2 for x in range(N)]
+
+@timer
+def mapcall(N):
+    return list(map((lambda x: x*2), range(N)))
+
+result=listcomp(5)
+
+listcomp(50000)
+listcomp(50000)
+listcomp(1000000)
+print(result)
+print(listcomp.alltime)
+print("")
+result=mapcall(5)
+mapcall(50000)
+mapcall(500000)
+mapcall(1000000)
+print(result)
+print(mapcall.alltime)
+
+
