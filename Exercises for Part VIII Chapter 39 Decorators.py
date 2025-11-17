dd3702c0a2b436f85949f97cb0615717fc5e6d25
SerjioMuz <serjiomora911@gmail.com>
2025-11-17 21:38:38 +0200

    Декораторы функций

--- Exercises for Part VIII Chapter 39 Decorators.py
+++ Exercises for Part VIII Chapter 39 Decorators.py
@@ -397,3 +397,114 @@ Y = Spam(99)
 print(X.attr, Y.attr)
 
 
+
+def singleton(aClass):
+    instance = None
+    def onCall(*args, **kwargs):
+        nonlocal instance
+        if instance == None:
+            instance = aClass(*args, **kwargs)
+        return instance
+    return onCall
+
+@singleton
+class Person:
+    def __init__(self, name, hours, rate):
+        self.name = name
+        self.hours = hours
+        self.rate = rate
+    def pay(self):
+        return self.hours * self.rate
+
+@singleton
+class Spam:
+    def __init__ (self, val):
+        self.attr = val
+
+bob = Person('Bob', 40, 10)
+print (bob.name, bob.pay())
+sue = Person('Sue', 50, 20)
+print (sue.name, sue.pay())
+X = Spam(val=42)
+Y = Spam(99)
+print(X.attr, Y.attr)
+
+
+
+
+
+
+
+
+
+
+class singleton:
+    def __init__ (self, aClass) :
+        self.aClass = aClass
+        self.instance =None
+    def __call__ (self, *args, **kwargs) :
+        if self.instance == None:
+            self.instance = self.aClass(*args, **kwargs)
+        return self.instance
+
+@singleton
+class Person:
+    def __init__(self, name, hours, rate):
+        self.name = name
+        self.hours = hours
+        self.rate = rate
+    def pay(self):
+        return self.hours * self.rate
+
+@singleton
+class Spam:
+    def __init__ (self, val):
+        self.attr = val
+
+bob = Person('Bob', 40, 10)
+print (bob.name, bob.pay())
+sue = Person('Sue', 50, 20)
+print (sue.name, sue.pay())
+X = Spam(val=42)
+Y = Spam(99)
+print(X.attr, Y.attr)
+
+
+
+class Wrapper:
+    def __init__ (self, object):
+        self.wrapped = object
+    def __getattr__(self, attrname):
+        print('Trace:', attrname)
+        return getattr(self.wrapped, attrname)
+
+
+
+
+"файл interfacertacer.py"
+def Tracer (aClass):
+    class Wrapper:
+        def __init__ (self, *args, **kargs):
+            self.fetches = 0
+            self.wrapped = aClass(*args, **kargs)
+        def __getattr__ (self, attrname):
+            print ('Trace: ' + attrname)
+            self.fetches += 1
+            return getattr(self.wrapped, attrname)
+    return Wrapper
+
+#if __name__ == '__main__' :
+@Tracer
+class Spam:
+    def display (self):
+        print('Spam! ' * 8)
+
+@Tracer
+class Person:
+    def __init__(self, name, hours, rate):
+        self.name = name
+        self.hours = hours
+        self.rate = rate
+    def pay(self):
+        return self.hours * self.rate
+
