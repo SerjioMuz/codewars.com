41f9d3329d74cc44fc15f6b4b8913cd208150ea5
SerjioMuz <serjiomora911@gmail.com>
2025-11-30 18:34:53 +0200

    Декораторы функций

--- Exercises for Part VIII Chapter 39 Decorators.py
+++ Exercises for Part VIII Chapter 39 Decorators.py
@@ -530,3 +530,88 @@ class Spam:
 class Person:
     def __init__ (self, name):
         self.name = name
+
+
+
+
+registry = {}
+def register(obj):
+    registry [obj .__name__ ] = obj
+    return obj
+
+@register
+def spam(x):
+    return(x**2)
+
+@register
+def ham(x):
+    return(x ** 3)
+@register
+class Eggs:
+    def __init__ (self, x):
+        self.data = x ** 4
+    def __str__ (self) :
+        return str(self.data)
+
+
+
+"Файл access1.py"
+traceMe = False
+def trace(*args):
+    if traceMe: print ('[' + ''.join(map(str, args)) + ']')
+def Private(*privates) :
+    def onDecorator(aClass):
+        class onlnstance:
+            def __init__ (self, *args, **kargs):
+                self.wrapped = aClass(*args, **kargs)
+            def __getattr__ (self, attr):
+                trace('get:' , attr)
+                if attr in privates:
+                    raise TypeError('private attribute fetch: ' + attr)
+                else:
+                    return getattr(self.wrapped, attr)
+
+            def __setattr__ (self, attr, value):
+                trace ('set:', attr, value)
+                if attr == 'wrapped' :
+                    self.__dict__[attr] = value
+                elif attr in privates:
+                    raise TypeError('private attribute change: ' + attr)
+                else:
+                    setattr (self .wrapped, attr, value)
+        return onlnstance
+    return onDecorator
+if __name__ == '__main__':
+    traceMe = True
+    @Private('data', 'size')
+    class Doubler:
+        def __init__ (self, label, start):
+            self.label = label
+            self.data= start
+        def size (self):
+            return len (self.data)
+        def double (self) :
+            for i in range(self.size()) :
+                self.data[i] = self.data[i] * 2
+        def display(self) :
+            print('%s => %s' % (self.label, self.data))
+
+    X = Doubler ('X is', [1, 2, 3])
+    Y = Doubler('Y is', [-10, -20, -30])
+
+
+    print(X.label)
+    X. display(); X.double(); X.display()
+    print(Y.label)
+    Y. display(); Y.double()
+    Y.label = 'Spam'
+    Y.display()
+
+    print(X.size())
+    print(X.data)
+    X.data = [1, 1, 1]
+    X.size = lambda S: 0
+    print(Y.data)
+    print(Y.size ())
+
+
