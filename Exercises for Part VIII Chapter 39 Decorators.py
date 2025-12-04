05e6d1ede75846327a30d30cc631b880195a9623
SerjioMuz <serjiomora911@gmail.com>
2025-12-04 19:13:26 +0200

    Декораторы функций

--- Exercises for Part VIII Chapter 39 Decorators.py
+++ Exercises for Part VIII Chapter 39 Decorators.py
@@ -702,3 +702,17 @@ print (sue .pay)
 
 
 
+def rangetest(func):
+    def onCall(*pargs, **kargs):
+        argchecks = func.__annotations__
+        print(argchecks)
+        for check in argchecks:
+            pass
+        return func(*pargs, **kargs)
+    return (onCall)
+
+@rangetest
+def func (a: (1, 5), b, c:(0.0, 1.0)):
+    print (a + b + c)
+
+func(1, 2, c=3)
