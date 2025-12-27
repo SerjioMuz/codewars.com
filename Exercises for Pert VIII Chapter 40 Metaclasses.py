50fcd12164c625b04d73f81cce5dc08a2920063d
SerjioMuz <serjiomora911@gmail.com>
2025-12-27 20:03:20 +0200

    Metaclasses

---
+++ Exercises for Pert VIII Chapter 40 Metaclasses.py
@@ -0,0 +1,40 @@
+class MetaOne(type):
+    def __new__ (meta, classname, supers, classdict):
+        print('In MetaOne.new:', meta, classname, supers, classdict, sep='\n...')
+        return type.__new__(meta, classname, supers, classdict)
+
+class Eggs: pass
+
+print('making class')
+
+class Spam(Eggs, metaclass=MetaOne):
+    data = 1
+    def meth(self, arg):
+        return self.data + arg
+
+print('making instance')
+X = Spam()
+print('data:', X.data, X.meth(2))
+
+
+
+class MetaTwo(type):
+    def __new__(meta, classname, supers, classdict):
+        print('In MetaTwo.new: ', classname, supers, classdict, sep='\n...')
+        return type.__new__(meta, classname, supers, classdict)
+    def __init__ (Class, classname, supers, classdict) :
+        print('In MetaTwo.init:', classname, supers, classdict, sep='\n...')
+        print('...init class object:', list(Class.__dict__.keys()))
+
+class Eggs: pass
+
+print('making class')
+
+class Spam(Eggs, metaclass=MetaTwo):
+    data = 1
+    def meth (self, arg):
+        return self.data + arg
+
+print('making instance')
+X = Spam()
+print('data:', X.data, X.meth(2))
